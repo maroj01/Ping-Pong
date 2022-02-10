3473435e49400e2e9ee1aa217de5c35d16ef0cd7
Adriana <adri.rojas990mail.com>
2022-02-10 16:03:47 -0500

    Se corrige varible ctx por contexto

--- Main.js
+++ Main.js
@@ -13,7 +13,7 @@ this.ball= null;
 
 self.Board.prototype = {
     get elements(){
-        var elements = this.bars;
+        var elements = this.bars.map(function(bar){ return bar; });;
         elements.push(this.ball);
         return elements;
     }
@@ -29,26 +29,44 @@ self.Board.prototype = {
 		this.speed_y = 0;
 		this.speed_x = 3;
 		this.board = board;
-		
+        this.direction = -1;	
+        this.bounce_angle = 0;
+		this.max_bounce_angle = Math.PI / 12;
+		this.speed = 3;
 
 		board.ball = this;
 		this.kind = "circle";	
 	}
-})();
+    //Método para mover la pelota
+    self.Ball.prototype = {
+		move: function(){
+			this.x += (this.speed_x * this.direction);
+			this.y += (this.speed_y);
+		},
+        get width(){
+			return this.radius * 2;
+		},
+		get height(){
+			return this.radius * 2;
+		}
 
+
+
+	}
+})();
 
 
 // Dibujar las barras
 (function(){
-self.Bar = function(x,y,width,height,board){
-this.x = x; // cordenadas
-this.y = y;
-this.width = width;
-this.height = height;
-this.board = board;
-this.board.bars.push(this);
-this.kind = "rectangle";
-this.speed = 10; // incrementa o decrementa la velocidad
+   self.Bar = function(x,y,width,height,board){
+   this.x = x; // cordenadas
+   this.y = y;
+   this.width = width;
+   this.height = height;
+   this.board = board;
+   this.board.bars.push(this);
+   this.kind = "rectangle";
+   this.speed = 10; // incrementa o decrementa la velocidad
 }
 
 //Mover las barras
@@ -69,7 +87,6 @@ this.y -= this.speed;
 //Vista // canvas para dibujar los elementos
 (function(){
     self.BoardView = function(canvas,board){
-
 		this.canvas = canvas;
 		this.canvas.width = board.width;
 		this.canvas.height = board.height;
@@ -77,12 +94,10 @@ this.y -= this.speed;
 		this.contexto = canvas.getContext("2d");  //getContexto es el metodo con el cual se puede dibujar en javascript
     }  
     
-    self.BoardView.prototype={
-        
+    self.BoardView.prototype={        
 		clean: function(){
-			this.ctx.clearRect(0,0,this.board.width,this.board.height);
+			this.contexto.clearRect(0,0,this.board.width,this.board.height);
 		},
-
         draw: function(){
             for (var i = this.board.elements.length - 1; i >= 0; i--) {
 				var el = this.board.elements[i];
@@ -90,31 +105,41 @@ this.y -= this.speed;
 				draw(this.contexto,el);
 			};
         },
+
+
+
+
+
+
         //Méto que ejecuta para dibujar y limpiar
         play: function(){
 			if(this.board.playing){
 				this.clean();
 				this.draw();
+                this.check_collisions();
+				this.board.ball.move();	                
 			}
-			
 		}
-
-
     }
+    
+
+
+
+
 
     // dibujar los diferentes elementos
     function draw(contexto, element){ // draw dibuja los diferentes elementos
-            switch (element.kind){
-                case "rectangle":
-                    contexto.fillRect(element.x,element.y,element.width,element.height);
-                    break;
-                case "circle": 
-                    ctx.beginPath();
-                    ctx.arc(element.x,element.y,element.radius,0,7);
-                    ctx.fill();
-                    ctx.closePath();
-                    break;
-             }
+        switch (element.kind){
+            case "rectangle":
+                contexto.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle": 
+            contexto.beginPath();
+            contexto.arc(element.x,element.y,element.radius,0,7);
+            contexto.fill();
+            contexto.closePath();
+                break;
+        }
     }
 })();
 
@@ -127,24 +152,27 @@ this.y -= this.speed;
    
    //Método que captura cuando el evento ocurre (mover las barras)
 document.addEventListener("keydow", function(ev){
-    ev.preventDefault();
+    
 if(ev.keyCode == 38){
+    ev.preventDefault();
     bar.up();
 }
 else if(ev.keyCode == 40){
+    ev.preventDefault();
     bar.down();
 }
 
 else if (ev.keyCode == 87){
+    ev.preventDefault();
     bar2.up();
 }
 else if(ev.keyCode == 83){
+    ev.preventDefault();
     bar2.down();
-
-console.log(""+bar); //""+bar //bar.toString()
 }
 });
 
+//board_view.draw();
 //Método que va actualizando el programa
 window.requestAnimationFrame(controler);
 // función que ejecuta todos los elementos
